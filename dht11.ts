c35d14dd23db45dedc9c9e7534f90bf076210797
zhang <1005941448@qq.com>
2020-09-21 11:36:18 +0800

    修改DHT的读

--- dht11.ts
+++ dht11.ts
@@ -1,55 +1,87 @@
 //% weight=0 color=#3CB371 icon="\uf043" block="dht11"
 namespace dht11 {
 
-    function signal_dht11(pin: DigitalPin): void {
-        pins.digitalWritePin(pin, 0);
-        basic.pause(18);
-        let i = pins.digitalReadPin(pin);
-        pins.setPull(pin, PinPullMode.PullUp);
+
+    export enum DHT11Type {
+        //% block="temperature(℃)" enumval=0
+        DHT11_temperature_C,
+
+        //% block="temperature(℉)" enumval=1
+        DHT11_temperature_F,
+
+        //% block="humidity(0~100)" enumval=2
+        DHT11_humidity,
     }
 
-    function dht11_read(pin: DigitalPin): number {
-        signal_dht11(pin);
+ //% blockId="readdht11" block="value of dht11 %dht11type| at pin %dht11pin"
+    export function dht11value(dht11type: DHT11Type, dht11pin: DigitalPin): number {
 
-        // Wait for response header to finish
-        while (pins.digitalReadPin(pin) == 1);
-        while (pins.digitalReadPin(pin) == 0);
-        while (pins.digitalReadPin(pin) == 1);
-
-        let value = 0;
-        let counter = 0;
-
-        for (let i = 0; i <= 32 - 1; i++) {
-            while (pins.digitalReadPin(pin) == 0);
-            counter = 0
-            while (pins.digitalReadPin(pin) == 1) {
-                counter += 1;
+    pins.digitalWritePin(dht11pin, 0)
+    basic.pause(18)
+    let i = pins.digitalReadPin(dht11pin)
+    pins.setPull(dht11pin, PinPullMode.PullUp);
+    switch (dht11type) {
+        case 0:
+            let dhtvalue1 = 0;
+            let dhtcounter1 = 0;
+            while (pins.digitalReadPin(dht11pin) == 1);
+            while (pins.digitalReadPin(dht11pin) == 0);
+            while (pins.digitalReadPin(dht11pin) == 1);
+            for (let i = 0; i <= 32 - 1; i++) {
+                while (pins.digitalReadPin(dht11pin) == 0);
+                dhtcounter1 = 0
+                while (pins.digitalReadPin(dht11pin) == 1) {
+                    dhtcounter1 += 1;
+                }
+                if (i > 15) {
+                    if (dhtcounter1 > 2) {
+                        dhtvalue1 = dhtvalue1 + (1 << (31 - i));
+                    }
+                }
             }
-            if (counter > 4) {
-                value = value + (1 << (31 - i));
+            return ((dhtvalue1 & 0x0000ff00) >> 8);
+            break;
+        case 1:
+            while (pins.digitalReadPin(dht11pin) == 1);
+            while (pins.digitalReadPin(dht11pin) == 0);
+            while (pins.digitalReadPin(dht11pin) == 1);
+            let dhtvalue = 0;
+            let dhtcounter = 0;
+            for (let i = 0; i <= 32 - 1; i++) {
+                while (pins.digitalReadPin(dht11pin) == 0);
+                dhtcounter = 0
+                while (pins.digitalReadPin(dht11pin) == 1) {
+                    dhtcounter += 1;
+                }
+                if (i > 15) {
+                    if (dhtcounter > 2) {
+                        dhtvalue = dhtvalue + (1 << (31 - i));
+                    }
+                }
             }
-        }
-        return value;
-    }
+            return Math.round((((dhtvalue & 0x0000ff00) >> 8) * 9 / 5) + 32);
+            break;
+        case 2:
+            while (pins.digitalReadPin(dht11pin) == 1);
+            while (pins.digitalReadPin(dht11pin) == 0);
+            while (pins.digitalReadPin(dht11pin) == 1);
 
-    export enum Dht11Result {
-        //% block="Celsius"
-        Celsius,
-        //% block="Fahrenheit"
-        Fahrenheit,
-        //% block="humidity"
-        humidity
-    }
+            let value = 0;
+            let counter = 0;
 
-    //% blockId=get_DHT11_value block="DHT11 set pin %pin_arg get %dhtResult" blockExternalInputs=true
-    //% pin_arg.fieldEditor="gridpicker" pin_arg.fieldOptions.columns=4
-    //% pin_arg.fieldOptions.tooltips="false" pin_arg.fieldOptions.width="300"
-    export function get_DHT11_value(pin_arg: DigitalPin, dhtResult: Dht11Result): number {
-        switch (dhtResult) {
-            case Dht11Result.Celsius: return (dht11_read(pin_arg) & 0x0000ff00) >> 8;
-            case Dht11Result.Fahrenheit: return ((dht11_read(pin_arg) & 0x0000ff00) >> 8) * 9 / 5 + 32;
-            case Dht11Result.humidity: return dht11_read(pin_arg) >> 24;
-            default: return 0;
-        }
+            for (let i = 0; i <= 8 - 1; i++) {
+                while (pins.digitalReadPin(dht11pin) == 0);
+                counter = 0
+                while (pins.digitalReadPin(dht11pin) == 1) {
+                    counter += 1;
+                }
+                if (counter > 3) {
+                    value = value + (1 << (7 - i));
+                }
+            }
+            return value;
+        default:
+            return 0;
     }
+}
 }
